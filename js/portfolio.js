0b1af5b2ee859b1055da975436adf3e03e359d3f
eralpkor <eralp.ny@gmail.com>
2019-04-10 12:14:30 -0400

    portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,33 +1,3 @@
-// d3.js stuff
-
-// var url =
-//   'https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/us.json';
-
-// var path = d3.geo.path();
-
-
-// var canvas = d3.select('.map-chart')
-//   .append('svg')
-//   .attr('width', 900)
-//   .attr('height', 700);
-
-// function d3Blows(error, topology) {
-//   if (error) throw error;
-//   // console.log('topojson', topology);
-  
-// }
-
-// d3.json(url).then(function (topology) {
-//   var geojson = topojson.feature(topology, topology.objects.counties);
-
-//   // console.log("geojson", geojson);
-//   // console.log(canvas);
-//   canvas.selectAll("path")
-//     .data(geojson.features)
-//     .enter()
-//     .append("path")
-//     .attr("d", path);
-// });
 
 
 
