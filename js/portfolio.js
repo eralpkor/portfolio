d653c72c2c25e33d1f827795c983c9c79cc716ef
eralpkor <eralp.ny@gmail.com>
2019-04-10 12:47:32 -0400

    portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,4 @@
 
-
-
-// Footer current year
-var year = new Date().getFullYear();
-document.getElementById('year').textContent = year;
-document.getElementById('norman-age').textContent = year - 2013;
-document.getElementById('sunny-age').textContent = year - 2018;
-
 // Home page type effect
 var typed = new Typed('#things', {
   strings: ["Frontend Developer", "Network Admin", "Test Engineer"],
@@ -14,5 +6,13 @@ var typed = new Typed('#things', {
   loop: true,
   showCursor: false
 });
+
+// Footer current year
+var year = new Date().getFullYear();
+document.getElementById('year').textContent = year;
+document.getElementById('norman-age').textContent = year - 2013;
+document.getElementById('sunny-age').textContent = year - 2018;
+
+
 
 console.log(year);
